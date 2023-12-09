7c261ee35ac2f340f21d145f2e27e7176ce00c5a
RITEFANG <1721985272@QQ.COM>
2023-12-09 10:00:37 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,30 +6,6 @@
 #
 
 LOCAL_PATH := device/xiaomi/unicorn
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.taro
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.taro \
-    libgptutils \
-    libz \
-    libcutils
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+PRODUCT_SHIPPING_API_LEVEL:=34
+PRODUCT_USE_DYNAMIC_PARTITIONS:=true
